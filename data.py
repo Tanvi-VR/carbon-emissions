a3d9e4ee9a990b41f6156a2fb143cdfb6ed18c0a
Tanvi Raman <tanvi.raman@berkeley.edu>
2025-09-28 22:23:30 -0700

    Add code to read climate data CSV

--- data.py
+++ data.py
@@ -1,17 +1,4 @@
-# import pandas as pd
-
-# climate_watch_GCP = pd.read_csv("CW_HistoricalEmissions_GCP.csv")
-# climate_watch_GCP.head() 
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-print("All libraries imported successfully!")
-print(f"Pandas version: {pd.__version__}")
-print(f"NumPy version: {np.__version__}")
-
-# Quick test
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-print("\nTest DataFrame:")
-print(df)
+climate_watch_GCP = pd.read_csv("ClimateWatch_HistoricalEmissions/CW_HistoricalEmissions_GCP.csv")
+climate_watch_GCP.head() 
